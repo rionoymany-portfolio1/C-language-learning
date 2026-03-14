e21c5c7297b77b858e3bf36ae4fe3321df95ff35
rionoymany-portfolio1 <rionoymany@gmail.com>
2026-03-14 15:01:15 +0700

    Delete hello.c

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> // Include standard input-output library for printing
-
-int main() {
-    // Printing the initialization of a professional workspace
-    printf("Rio's Strategic Terminal v1.0\n"); // Display name and version
-    printf("Status: Environment Ready for Financial & Security Analysis\n"); 
-    return 0; // Exit program successfully
-}
